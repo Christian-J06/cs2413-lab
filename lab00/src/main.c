4e323f65cbf7692a0ebe160029a6a386907e4ff1
Christian-J06 <Christian_J2006@icloud.com>
2026-01-28 10:25:48 -0600

    Learn git again

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,9 +4,9 @@ int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
-    printf("Name: YOUR_NAME_HERE\n");
-    printf("R#: ****XXXXX\n");
-    printf("Section: XXX\n");
+    printf("Name: Christian Johnson:\n");
+    printf("R#: 11223344\n");
+    printf("Section: 002\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
     printf("I have made a change!\n");
 
