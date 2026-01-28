cbb247a53f56ebfc08ade6155f8e4dc1cc852ca7
Christian-J06 <Christian_J2006@icloud.com>
2026-01-28 10:33:18 -0600

    Changes to reflect section and R#

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -2,15 +2,8 @@
 
 int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
-
-    // TODO: Replace the placeholder information below
-<<<<<<< HEAD
     printf("Name: Christian Johnson:\n");
     printf("R#: 11223344\n");
-=======
-    printf("Name: Shawn\n");
-    printf("R#: 11714613\n");
->>>>>>> 831b4d4dae534af32db089278619ba1d4d88963c
     printf("Section: 002\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
     printf("I have made a change!\n");
