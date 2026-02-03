5122d5bfbb4040ec8bc988787fd08c4fd9dcea3f
Christian-J06 <Christian_J2006@icloud.com>
2026-02-03 08:45:57 -0600

    Lab01 two_sum complete

--- lab01/src/two_sum.c
+++ lab01/src/two_sum.c
@@ -2,6 +2,24 @@
 
 int two_sum(const int* nums, int n, int target, int* out_i, int* out_j) {
 
+    for(int i = 0; i < n; i++)
+    {
+        if (n<2)
+        {
+            return 0;
+        }
+        for(int j = i+1; j < n; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                *out_i = i;
+                *out_j = j;
+                return 1;
+            }
+        }
+    }
+        
+
     // TODO: implement Two Sum.
     // Requirements:
     //  - find i < j such that nums[i] + nums[j] == target
