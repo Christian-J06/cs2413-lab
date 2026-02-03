50574d738c6d7a785f7de158078aea4dd5de8d55
Christian-J06 <Christian_J2006@icloud.com>
2026-02-03 09:06:11 -0600

    Lab01 three_sum_sorted complete

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -1,7 +1,25 @@
 #include "three_sum_sorted.h"
 
 int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, int* out_k) {
-    
+    if (!nums || !out_i || !out_j || !out_k ||n < 2){
+    return 0;
+    }
+
+    for(int i = 0; i < n-2; i++){
+        for(int j = i+1; j < n-1; j++){
+            for(int k = i+2; k < n; k++)
+            if (nums[i] + nums[j] + nums[k] == 0){
+                *out_i = i;
+                *out_j = j;
+                *out_k = k;
+                return 1;
+            }
+            
+        }
+    }
+    // TODO: implement two sum solution for sorted arrays.
+
+    return 0;
     // implement your code here
 
     return 0;
